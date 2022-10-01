90de67372b4c9c4fccb570a9e47068177b00ff56
ApoloGH <apolo-gh@users.noreply.github.com>
2022-10-01 12:03:05 -0400

    feat: implement steps

---
+++ index.js
@@ -0,0 +1,20 @@
+const steps = (...list) => ({
+  start: async (args) =>
+    list.reduce(async (prevStep, operation) => {
+      const prevStepResult = await prevStep;
+      if (Array.isArray(operation)) {
+        return steps(...operation).start(prevStepResult);
+      } else if (typeof operation === "function") {
+        const { exit, ...rest } = prevStepResult || {};
+        if (!exit) {
+          const result = await operation(rest);
+          return { ...rest, ...result };
+        }
+        return prevStepResult;
+      }
+      throw new Error("Invalid operation type");
+    }, Promise.resolve(args)),
+});
+
+const noop = async () => {};
+const exit = async () => ({ exit: false });
