678a54028946a64120c7b4038fa23d28f8401cd6
ApoloGH <apolo-gh@users.noreply.github.com>
2022-10-01 13:59:15 -0400

    chore: test pre-commit hooks

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ export const steps = (...list) => ({
     }, Promise.resolve(args)),
 });
 
-console.log('test')
+console.log('test');
 
 export const noop = async () => {};
 export const exit = async () => ({ exit: false });
